6396dab9b11f2fa32c0cd214f01ff9b033e0b9db
marianalizz001 <marianalizz001@hotmail.com>
2019-11-26 22:41:26 -0600

    Mariana
    Cambios inventario

--- InventarioAltaPHP.php
+++ InventarioAltaPHP.php
@@ -1,11 +1,11 @@
-//<?php
- 	include("Conexion.php");
-    include("loginPHP.php");
- 	$consulta= $conexion->prepare("insert into producto(nombre,precio,existencia,Usuario_idUsuario,fecha) values (?,?,?,?,?)");
-    $idUsuario = $_GET['idUsuario'];
-    echo $idUsuario;
-    $fecha=getdate();
- 	$consulta->bind_param('siiid',$_REQUEST['nombre-producto'], $_REQUEST['precio'],$_REQUEST['existencia'],$idUsuario,$_REQUEST['fecha']);
+<?php
+    session_start();
+     include('Conexion.php');
+
+    $idUsuario= $_SESSION['id'];
+ 	$consulta= $conexion->prepare("insert into producto(nombre,precio,existencia,Usuario_idUsuario,fecha) values (?,?,?,?,now())");
+    
+ 	$consulta->bind_param('siii',$_REQUEST['nombre-producto'], $_REQUEST['precio'],$_REQUEST['existencia'],$idUsuario);
 
  	if($consulta->execute()){
 		header('location: inventario_alta.php');
