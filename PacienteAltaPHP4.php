76ffef319fc60b30416d0344d87c913c5eed1ece
ivtmarquez <57645487+ivtmarquez@users.noreply.github.com>
2019-11-27 18:48:28 -0600

    Update PacienteAltaPHP4.php

--- PacienteAltaPHP4.php
+++ PacienteAltaPHP4.php
@@ -2,17 +2,23 @@
     include("Conexion.php");
     
     $idUsuario = $_REQUEST['idUsuario'];
+
+    unset($_POST['btnEnviar']);
     $var_json = json_encode($_POST);
 
-    $sql = "UPDATE Detalle_paciente SET ant_fam = '.$var_json.' WHERE Usuario_idUsuario = $idUsuario";
+    // var_dump($var_json); 
+
+    $sql = "UPDATE Detalle_paciente SET ant_fam = '$var_json' WHERE Usuario_idUsuario = $idUsuario";
 
     if (mysqli_query($conexion, $sql)) {
-        echo "New record created successfully";
+        echo "Bien";
         header('location: PacienteAlta4.php');
     } else {
         echo "Error: " . "<br>" . mysqli_error($conexion);
     }
 
     mysqli_close($conexion);
+
+    
 
 ?>
