12102132efeb7e3fcb563c4b7705298b65cb96a6
ivtmarquez <57645487+ivtmarquez@users.noreply.github.com>
2019-11-30 13:07:33 -0600

    Diseño paciente

--- PacienteEditar.php
+++ PacienteEditar.php
@@ -21,8 +21,73 @@
   </head>
 
 <body>
-  
 
+<div class="accordion" id="accordionExample">
+  <div class="card">
+    <div class="card-header" id="uno">
+      <h2 class="mb-0">
+        <button class="btn btn-outline-secondary" type="button" style="width: 300px;"  data-toggle="collapse" data-target="#collapseuno" aria-expanded="false" aria-controls="collapseuno">
+          H I S T O R I A  -  C L I N I C A
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseuno" class="collapse" aria-labelledby="headinguno" data-parent="#accordionExample">
+      <div class="card-body">
+          Aqui ponemos el PDF de la historia clinica
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header" id="headingcuatro">
+      <h2 class="mb-0">
+        <button class="btn btn-outline-secondary" type="button" style="width: 300px;"  data-toggle="collapse" data-target="#collapsecuatro" aria-expanded="false" aria-controls="collapsecuatro">
+          P E R F I L
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapsecuatro" class="collapse" aria-labelledby="headingcuatro" data-parent="#accordionExample">
+      <div class="card-body">
+          
+      </div>
+    </div>
+  </div>
+
+
+  <div class="card">
+    <div class="card-header" id="headingdos">
+      <h2 class="mb-0">
+        <button class="btn btn-outline-secondary" type="button" style="width: 300px;"  data-toggle="collapse" data-target="#collapsedos" aria-expanded="false" aria-controls="collapsedos">
+          C I T A S
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapsedos" class="collapse" aria-labelledby="headingdos" data-parent="#accordionExample">
+      <div class="card-body">
+          
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header" id="headingtres">
+      <h2 class="mb-0">
+        <button class="btn btn-outline-secondary" type="button" style="width: 300px;"  data-toggle="collapse" data-target="#collapsetres" aria-expanded="false" aria-controls="collapsetres">
+          P A G O S
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapsetres" class="collapse" aria-labelledby="headingtres" data-parent="#accordionExample">
+      <div class="card-body">
+          
+      </div>
+    </div>
+  </div>
+</div>
 </body>
   
 </html>
