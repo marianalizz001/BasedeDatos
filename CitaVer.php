01f1c6d294455c18d54d19b946fba18fe8e8a316
JoannaLoza <57685440+JoannaLoza@users.noreply.github.com>
2019-12-05 15:41:06 -0600

    Joanna

--- CitaVer.php
+++ CitaVer.php
@@ -26,21 +26,35 @@
     <div class="container" id="registro">
         <div class="row">
             <div class="col-12" id="barra_servicio">
-                <A class="h2 align-middle text-center" name="servicios" id="servicio">Añadir Producto al Inventario</A>
+                <A class="h2 align-middle text-center" name="servicios" id="servicio">Agendar Cita</A>
             </div>
         </div>
         <br><br>
+        <?php
+          $instruccion = ("SELECT idUsuario FROM Usuario WHERE usuario=?");
+          $consulta = $conexion->prepare($instruccion);
+          $consulta->bind_param("s", $usuario);
+      
+          if($consulta->execute()){
+              $consulta->bind_result($idUsuario);
+              $consulta->fetch();
+             // echo $idUsuario;
+          }else{
+              echo $conexion -> error;
+          }           
+?>
         <div class="container">
             <br><br>
             <div class="row">
                 <div class="col-6">
-                    <form action="eventosPac.php" method="post">
+                    <form action="eventosPac.php"  method="POST  enctype="multipart/form-data">
                         <div class="form-group row">
                             <div class="col-6">
+                            <input type="hidden" value="<?php echo $_SESSION['id'];?>" name="idUsuario" id="idUsuario" >
                                 <label for="nombre-paciente" style="font-size:20px;color: rgba(144, 12, 52);"> Nombre del Usuario: </label>
                             </div>
                             <div class="col-6">
-                                <input type="text" class="form-control" id="nom_paciente" name="nom_paciente">
+                                <input type="text" class="form-control" readonly="readonly"  name="nom_paciente" id="nom_paciente"  value="<?php echo $_SESSION['nombre']; ?>">
                             </div>
                         </div>
                         <div class="form-group row">
@@ -48,7 +62,7 @@
                                 <label for="fecha" style="font-size:20px;color: rgba(144, 12, 52);"> Fecha de la cita: </label>
                             </div>
                             <div class="col-6">
-                                <input type="date" class="form-control" id="fecha_cita" >
+                                <input type="date" class="form-control" name="fecha_cita" id="fecha_cita" min="2019-12-01" >
                             </div>
                         </div>
                         <div class="form-group row">
@@ -72,7 +86,7 @@
                                 <label for="titulo" style="font-size:20px;color: rgba(144, 12, 52);"> Servicios: </label>
                             </div>
                             <div class="col-6">
-                            <select class="form-control" name="txtTitulo" id="txtTitulo">
+                            <select class="form-control" name="txtTitulo" id="txtHora">
                             <option>Ortodoncia</option>
                             <option>Protesis</option>
                             <option>Estetica dental</option>
@@ -88,7 +102,7 @@
                             </div>
                         </div>
                         <br><br>
-                        <input type="button" class="btn btn-success btn-lg btn-block" type="submit" value="Agendar Cita" name="btnEnviar">
+                        <input class="btn btn-success btn-lg btn-block" type="submit" value="Agendar Cita" name="btnEnviar">
                      
                     </form>
                     
@@ -97,6 +111,95 @@
                         <img src="img/calendario.jpg" alt="Calendario" width="250px">
                     </div>
             </div>
+
+            <script>
+        /*Recolecta los datos manda llamar a la funcion Recolectar datos y envia la instruccion de lo que se desea hacer*/
+        var NuevoEvento;
+        $('#btnEnviar').click(function() {
+            RecolectarDatos();
+            EnviarInformacion('agregar', NuevoEvento);
+        });
+
+
+        function RecolectarDatos() {
+            /*Recolecta los datos de los inputs para luego hacer las consultas*/
+            if ( $('#txtTitulo').val() == "Ortodoncia"){
+               $color="#0080ff";
+            }
+            if ( $('#txtTitulo').val() == "Protesis"){
+               $color="#ff8000";
+            }
+            if ( $('#txtTitulo').val() == "Estetica dental"){
+               $color="#ce00ce";
+            }
+            if ( $('#txtTitulo').val() == "Higiene"){
+               $color="#00df52";
+            }
+            if ( $('#txtTitulo').val() == "Prevencion"){
+               $color="#004080";
+            }
+            if ( $('#txtTitulo').val() == "Odontopediatria"){
+               $color="#d5006b";
+            }
+            if ( $('#txtTitulo').val() == "Endodoncia"){
+               $color="#ff0606";
+            }
+            if ( $('#txtTitulo').val() == "Peridoncia"){
+               $color="#1B743A";
+            }
+            if ( $('#txtTitulo').val() == "Cirugia dental"){
+               $color="#a80b0b";
+            }
+            if ( $('#txtTitulo').val() == "Otros"){
+               $color="#000000";
+            }
+             
+            
+                     
+            NuevoEvento = {
+  
+                id: $('#idUsuario').val(),
+                title: $('#txtTitulo').val(),
+                nombre: $('#txtNombre').val(),
+                start: $('#txtFecha').val() + " " + $('#txtHora').val(),
+                color: $color,
+                textColor: "#FFFFFF",
+                end: $('#txtFecha').val() + " " + $('#txtHora').val()
+            };
+            
+        }
+
+        function EnviarInformacion(accion, objEvento) {
+            /*Envia la info usando ajax*/
+            $.ajax({
+                type: 'POST',
+                url: 'eventosPac.php?accion=' + accion,
+                data: objEvento,
+                success: function(msj) {
+                    if (msj) {
+                        $('#Calendario').fullCalendar('refetchEvents');
+                        if (!modal) {
+                            $("#ModalEventos").modal('toggle');
+                        }
+                    }
+                },
+                error: function() {
+                    alert: ('Hay un error...');
+                }
+
+            });
+        }
+
+        function limpiarFormulario() {
+            /*Limpia el formulario */
+            $('#txtID').val('');
+            $('#txtTitulo').val('');
+            $('#txtNombre').val('');
+            $('#txtColor').val('');
+            $('#txtHora').val('');
+        }
+
+    </script>
 
             <br><br><br>
             <br>
--- eventosPac.php
+++ eventosPac.php
@@ -1,75 +1,104 @@
+<link href="css/sweetalert.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/functions.js"></script>
+<script src="js/sweetalert.min.js"></script>
+
 <?php
-/*Hace la conexion a la base de datos*/
-    header('Content-Type: application/json');
-    $pdo=new PDO("mysql:dbname=consultorio;host:127.0.0.1","root","");
-    $accion = (isset($_GET['accion']))?$_GET['accion']:'leer';
+    session_start();
+    include('Conexion.php');
 
-    switch($accion){
-        case 'agregar':
-            /*Agrega los valores a la BD*/
-            $sentenciaSQL = $pdo->prepare("INSERT INTO
-            cita(title,nombre,color,textColor,start,end)
-            VALUES(:title,:nombre,:color,:textColor,:start,:end)");
-            
-            $respuesta=$sentenciaSQL->execute(array(
-               "title" => $_POST['title'],
-                "nombre" => $_POST['nombre'],
-                "color" => $_POST['color'],
-                "textColor" => $_POST['textColor'],
-                "start" => $_POST['start'],
-                "end" => $_POST['end']
-            ));
+	$idUsuario= $_SESSION['id'];
+	//echo $idUsuario;
+	$Nombre = $_SESSION['nombre'];
+	//echo $Nombre;
+	$Fecha=$_REQUEST['fecha_cita'];
+	//echo $Fecha;
+	$Hora= $_REQUEST['txtHora'];
+	//echo $Hora;
+	$Titulo= $_REQUEST['txtTitulo'];
+	//echo $Titulo;
+	$Fecha_Inicial = $Fecha." ".$Hora;
+	//echo $Fecha_Inicial;
+	$Fecha_Final = $Fecha." ".$Hora;
+	//echo $Fecha_Final;
 
-            echo json_encode($respuesta);
-            $conexion = new mysqli ('localhost','root','','Consultorio');
-            $sql1="call p4()";
-            $result1=mysqli_query($conexion,$sql1);
-            break;
-            
-        case 'eliminar':
-            $respuesta=false;
-            
-            if(isset($_POST['id'])){
-                $sentenciaSQL= $pdo->prepare("DELETE FROM
-                cita WHERE ID=:ID");
-                $respuesta= $sentenciaSQL->execute(array("ID"=>$_POST['id']));
-            }
-            echo json_encode($respuesta);
-            break; 
-            
-        case 'modificar':
-            //Instruccion para modificar
-            //echo "Instruccion modificar";
-            $sentenciaSQL=$pdo->prepare("UPDATE cita SET
-            title=:title,
-            nombre=:nombre,
-            color=:color,
-            textColor=:textColor,
-            start=:start,
-            end=:end
-            WHERE ID=:ID
-            ");
-            
-            $respuesta=$sentenciaSQL->execute(array(
-                "ID"=>$_POST['id'],
-                "title" => $_POST['title'],
-                "nombre" => $_POST['nombre'],
-                "color" => $_POST['color'],
-                "textColor" => $_POST['textColor'],
-                "start" => $_POST['start'],
-                "end" => $_POST['end']
-            ));
-            echo json_encode($respuesta);
-            break;
-            
-        default:
-            //seleccionar los eventos del calendario para mostrarlos siempre
-            $sentenciaSQL= $pdo->prepare("SELECT * FROM cita");
-            $sentenciaSQL->execute();
+	if ( $Titulo == "Ortodoncia"){
+		$color="#0080ff";
+	 }
+	 if ( $Titulo == "Protesis"){
+		$color="#ff8000";
+	 }
+	 if ( $Titulo == "Estetica dental"){
+		$color="#ce00ce";
+	 }
+	 if ( $Titulo == "Higiene"){
+		$color="#00df52";
+	 }
+	 if ( $Titulo == "Prevencion"){
+		$color="#004080";
+	 }
+	 if ( $Titulo == "Odontopediatria"){
+		$color="#d5006b";
+	 }
+	 if (	$Titulo == "Endodoncia"){
+		$color="#ff0606";
+	 }
+	 if ( $Titulo == "Peridoncia"){
+		$color="#1B743A";
+	 }
+	 if ( $Titulo == "Cirugia dental"){
+		$color="#a80b0b";
+	 }
+	 if ( $Titulo == "Otros"){
+		$color="#000000";
+	 }
 
-            $resultado= $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-            echo json_encode($resultado);
-            break;
-    }
+	 $TextColor="#FFFFFF";
+ 	$consulta= $conexion->prepare("INSERT into cita(title,nombre,color,textColor,start,end,usuario_idUsuario) values (?,?,?,?,?,?,?)");
     
+ 	$consulta->bind_param('ssssssi',$Titulo,$Nombre,$color,$TextColor,$Fecha_Inicial,$Fecha_Final,$idUsuario);
+
+ 	if($consulta->execute()){
+		?>
+            <script>
+            jQuery(function() {
+                swal({   
+                    title: "¡Bien!",   
+                    text: "Se han guardado los datos",   
+                    type: "success",    
+                    confirmButtonColor: "#696565",   
+                    confirmButtonText: "Ok",   
+                    closeOnConfirm: false}, 
+
+                    function(isConfirm){   
+                        if (isConfirm) {     
+                            window.location.href = "CitaVer.php";
+                        }
+                    });
+            });
+            </script>
+        <?php 
+ 	}else{	
+        ?>
+                <script>
+                jQuery(function() {
+                    swal({   
+                        title: "¡Error!",   
+                        text: "No se han actualizado los datos",   
+                        type: "error",    
+                        confirmButtonColor: "#DD6B55",   
+                        confirmButtonText: "Intentar de nuevo",   
+                        closeOnConfirm: false}, 
+    
+                        function(isConfirm){   
+                            if (isConfirm) {     
+                                window.location.href = "javascript:window.history.back()";
+                            }
+                        });
+                });
+                </script>
+            <?php
+ 	}
+
 ?>
