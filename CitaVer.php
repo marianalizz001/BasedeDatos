5473f2008849bd1786e90153beab5f40ad9a97d3
JoannaLoza <57685440+JoannaLoza@users.noreply.github.com>
2019-12-04 21:18:48 -0600

    Joanna

--- CitaVer.php
+++ CitaVer.php
@@ -1,65 +1,78 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="img/favicon.png">
-    
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Anton|Dosis:400,800" rel="stylesheet">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet"/>
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet" />
 
     <link rel="stylesheet" href="css/style.css">
 
-    <title>Dra.YazminNajera | Agendar Cita</title>
+    <title>Dra.YazminNajera | Inventario</title>
 
     <?php include("navbar.php"); ?>
     <br>
 
 </head>
-<body>
-<div class="container" id="registro">
-  <div class="row">
-    <div class="col-12" id="barra_servicio">
-        <A class="h2 align-middle text-center" name="servicios" id="servicio">Agendar Cita</A>
-    </div>
-  </div>
-  <br>
-  <br>
-  <br>
 
-  <form>
-  <div class="form-group row">
-    <label class="col-sm-2 col-form-label">Nombre</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="nombre" placeholder="Nombre">
-    </div>
-  </div>
-  <div class="form-group row">
-    <label class="col-sm-2 col-form-label">Fecha</label>
-    <div class="col-sm-10">
-      <input type="date" class="form-control" id="fecha"  min="2019-01-01" max="2020-12-31">
-    </div>
-  </div>
-  <div class="form-group row">      
-                            <label  class="col-sm-2 col-form-label">Hora de la cita:</label>
-                            <div class="col-sm-10">
-                            <select class="form-control" name="hora" id="hora">
-                                <option>10:00-11:00</option>
-                                <option>11:00-12:00</option>
-                                <option>16:00-17:00</option>
-                                <option>17:00-18:00</option>
-                                <option>18:00-19:00</option>
-                            </select>
+<body>
+    <div class="container" id="registro">
+        <div class="row">
+            <div class="col-12" id="barra_servicio">
+                <A class="h2 align-middle text-center" name="servicios" id="servicio">Añadir Producto al Inventario</A>
+            </div>
+        </div>
+        <br><br>
+        <div class="container">
+            <br><br>
+            <div class="row">
+                <div class="col-6">
+                    <form action="eventosPac.php" method="post">
+                        <div class="form-group row">
+                            <div class="col-6">
+                                <label for="nombre-paciente" style="font-size:20px;color: rgba(144, 12, 52);"> Nombre del Usuario: </label>
                             </div>
-</div>
-    
-    <div class="form-group row">
-                        <label  class="col-sm-2 col-form-label">Servicios:</label>
-                        <div class="col-sm-10">
-                        <select class="form-control" name="txtTitulo" id="txtTitulo">
+                            <div class="col-6">
+                                <input type="text" class="form-control" id="nom_paciente" name="nom_paciente">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-6">
+                                <label for="fecha" style="font-size:20px;color: rgba(144, 12, 52);"> Fecha de la cita: </label>
+                            </div>
+                            <div class="col-6">
+                                <input type="date" class="form-control" id="fecha_cita" >
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-6">
+                                <label for="hora" style="font-size:20px;color: rgba(144, 12, 52);"> Hora de la cita: </label>
+                            </div>
+                            <div class="col-6">
+                            <select class="form-control" name="txtHora" id="txtHora">
+                                <option>10:00:00</option>
+                                <option>11:00:00</option>
+                                <option>16:00:00</option>
+                                <option>17:00:00</option>
+                                <option>18:00:00</option>
+                                <option>19:00:00</option>
+                            </select>
+
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-6">
+                                <label for="titulo" style="font-size:20px;color: rgba(144, 12, 52);"> Servicios: </label>
+                            </div>
+                            <div class="col-6">
+                            <select class="form-control" name="txtTitulo" id="txtTitulo">
                             <option>Ortodoncia</option>
                             <option>Protesis</option>
                             <option>Estetica dental</option>
@@ -71,21 +84,29 @@
                             <option>Cirugia dental</option>
                             <option>Otros</option>
                         </select>
+
+                            </div>
+                        </div>
+                        <br><br>
+                        <input type="button" class="btn btn-success btn-lg btn-block" type="submit" value="Agendar Cita" name="btnEnviar">
+                     
+                    </form>
+                    
+                </div>
+                <div class="col-6">
+                        <img src="img/calendario.jpg" alt="Calendario" width="250px">
                     </div>
-                    <br>
-  </div>
+            </div>
 
- 
+            <br><br><br>
+            <br>
+            <br>
+            <br>
+        </div>
 
-  <div class="form-group row">
-    <div class="col-sm-10">
-     <button type="submit" class="btn btn-info btn-lg btn-block">Sign in</button>
     </div>
-  </div>
-  
-</form>
-  
-</div>
+
 </body>
+
 </html>
 <?php include("footer.php"); ?>
