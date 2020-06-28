7759015d2346a5193208fd5c2437ef91fc2b3d03
aestrada17 <57645781+aestrada17@users.noreply.github.com>
2020-06-27 20:52:56 -0500

    Angie
    Reporte empleados

--- Reportes/ListadoEmpleados.php
+++ Reportes/ListadoEmpleados.php
@@ -23,61 +23,71 @@
 		}		
     }
     
+    $consulta = $bd->Usuario->find(
+        [
+            'tipo_usuario' => 'E',
+            'f_baja' => array('$exists' => false)
+        ]
+        );
 
-    $query = "SELECT tipo_usuario, usuario, f_alta, rfc, salario, nombre, apPat, apMat, f_nac, genero, calle, no_ext, no_int, colonia, cp, correo,
+   /* $query = "SELECT tipo_usuario, usuario, f_alta, rfc, salario, nombre, apPat, apMat, f_nac, genero, calle, no_ext, no_int, colonia, cp, correo,
      telefono FROM usuario WHERE tipo_usuario = 'E'";
 	
-    $resultado = mysqli_query($conexion, $query);
+    $resultado = mysqli_query($conexion, $query);*/
 	
 	$pdf = new PDF();
 	$pdf->AliasNbPages();
     $pdf->AddPage();
     
-    while($row = $resultado->fetch_assoc()){
+    foreach($consulta as $act ){
+        $idUsuario = $act['_id'];
+        $usuario = $act['usuario'];
+        $nombre = $act['nombre'];
+        $apPat = $act['apPat'];
+        $apMat = $act['apMat'];
+        $genero = $act['genero'];
+        $correo = $act['correo'];
+        $f_nac = $act['f_nac'];
+        $telefono = $act['telefono'];
+        $rfc = $act['rfc'];
+        $salario = $act['salario'];
+
         $pdf->SetTextColor(0,0,0);
         $pdf->SetFont('Arial','B',12);
         $pdf->SetFillColor(232,232,232);
 
         $pdf->Cell(50,6,'Usuario ',1,0,'L',1);
         $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['usuario']),1,1,'L');
-
-        $pdf->Cell(50,6,'Fecha de alta ',1,0,'L',1);
-        $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['f_alta']),1,1,'L');
+        $pdf->Cell(140,6,utf8_decode($usuario),1,1,'L');
 
         $pdf->Cell(50,6,'Salario ',1,0,'L',1);
         $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['salario']),1,1,'L');
+        $pdf->Cell(140,6,utf8_decode($salario),1,1,'L');
 
         $pdf->Cell(50,6,'RFC ',1,0,'L',1);
         $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['rfc']),1,1,'L');
+        $pdf->Cell(140,6,utf8_decode($rfc),1,1,'L');
 
         $pdf->Cell(50,6,'Nombre ',1,0,'L',1);
         $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['nombre']." ".$row['apPat']." ". $row['apMat']),1,1,'L');
+        $pdf->Cell(140,6,utf8_decode($nombre." ".$apPat." ". $apMat),1,1,'L');
 
         $pdf->Cell(50,6,'Fecha de Nacimiento ',1,0,'L',1);
         $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['f_nac']),1,1,'L');
+        $pdf->Cell(140,6,utf8_decode($f_nac),1,1,'L');
 
         $pdf->Cell(50,6,'Genero ',1,0,'L',1);
         $pdf->SetFillColor(232,232,232);
-        if ($row['genero'] == 'F') $pdf->Cell(140,6,utf8_decode('Femenino'),1,1,'L');
-        if ($row['genero'] == 'M') $pdf->Cell(140,6,utf8_decode('Masculino'),1,1,'L');
+        if ($genero == "F") $pdf->Cell(140,6,utf8_decode('Femenino'),1,1,'L');
+        if ($genero == "M") $pdf->Cell(140,6,utf8_decode('Masculino'),1,1,'L');
         
         $pdf->Cell(50,6,'Correo ',1,0,'L',1);
         $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['correo']),1,1,'L');
+        $pdf->Cell(140,6,utf8_decode($correo),1,1,'L');
 
         $pdf->Cell(50,6,utf8_decode('Teléfono '),1,0,'L',1);
         $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['telefono']),1,1,'L');
-
-        $pdf->Cell(50,6,'Domicilio ',1,0,'L',1);
-        $pdf->SetFillColor(232,232,232);
-        $pdf->Cell(140,6,utf8_decode($row['calle']. " ". $row['no_ext']. "". $row['no_int']." ". $row['colonia']. " " . $row['cp']),1,1,'L');
+        $pdf->Cell(140,6,utf8_decode($telefono),1,1,'L');
         
         $pdf->Ln();
     }
